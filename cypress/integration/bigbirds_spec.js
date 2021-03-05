64ed01a368aeae976ed439f8830a5f72f40fb7d9
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-03-05 09:44:06 -0600

    Test for parts of the Highlight Component

--- cypress/integration/bigbirds_spec.js
+++ cypress/integration/bigbirds_spec.js
@@ -11,6 +11,12 @@ describe('BigBirds', () => {
     })
 
     it('Should have a bird of the day highlight', () => {
+      cy.get('main .highlight').should('be.visible')
+        .get('article')
+          .get('img').should('be.visible')
+          .get('h3').should('be.visible')
+          .get('h4').should('be.visible')
+          .get('a').should('be.visible').and('have.attr', 'href', 'https://www.allaboutbirds.org/guide/Brown_Creeper/')
 
     })
 

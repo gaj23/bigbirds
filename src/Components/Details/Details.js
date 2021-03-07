cfc71e7aefa6ea1d784740ddadc8748b94a318d0
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-03-07 16:20:30 -0600

    Pseudo code logic for finding a birds image

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './Details.css';
+import images from '../../data/imagesData';
 
 
 const Details = () => {
   const location = useLocation();
   const { speciesCode, comName, sciName } = location.state
+
+  const img = images.find(bird => bird[].includes(speciesCode))
+  console.log('our bird', img, speciesCode)
   return (
     <section>
       <p>About the {comName}</p>

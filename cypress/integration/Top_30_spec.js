94de915b23dd5ca1b8dce5823906d50bd1f2e09a
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-03-08 20:35:08 -0600

    Test top 30 birds & navigation

--- cypress/integration/Top_30_spec.js
+++ cypress/integration/Top_30_spec.js
@@ -6,7 +6,25 @@ describe('Top 30', () => {
   })
 
   it('Should give instructions to the user', () => {
+    cy.wait(2000)
     cy.get('main nav').contains('Top').click()
-    cy.get()
+      .get('section').contains('Seen it?')
   })
+
+  it('Should have ability to check off and on birds', () => {
+    cy.wait(2000)
+    cy.get('main nav').contains('Top').click()
+      .get('section article table tbody tr td input[type=checkbox]').first().click()
+      .get('section article table tbody tr td input[type=checkbox]').first().click()
+  })
+
+  it('Should have the ability to go into a bird\'s details', () => {
+    cy.wait(2000)
+    cy.get('main nav').contains('Top').click()
+      .get('section article table tbody tr td a').first().click()
+      .location().should(loc => {
+      expect(loc.pathname).to.eq('/bird_details/daejun')
+    })
+  })
+
 })
